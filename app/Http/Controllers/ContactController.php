d2441c41467befc967986149397c040d0c39ff19
keiwebcreate <keiwebcreate20240704@gmail.com>
2026-05-30 16:09:51 +0900

    ContactRequestを使ってバリデーションを設定しました

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,22 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use Illuminate\Support\Facades\Log;
+
 
 class ContactController extends Controller
 {
     public function index() {
-        
+        return view('contact.index');
     }
 
-    public function sendMail(Request $request) {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'name_kana' => ['required', 'string', 'max:255', 'regex:/^[ァ-ロワンヴー]*$/u'],
-            'phone' => ['nullable', 'regex:/^0(\d-?\d{4}|\d{2}-?\d{3}|\d{3}-?\d{2}|\d{4}-?\d|\d0-?\d{4})-?\d{4}$/'],
-            'email' => ['required', 'email'],
-            'body' => ['required', 'string', 'max:2000'],
-        ]);
+    public function sendMail(ContactRequest $request) {
+        $validated = $request->validated();
 
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)
