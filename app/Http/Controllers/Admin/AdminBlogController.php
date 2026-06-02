4114724258ed2f2da7270fdd74241440f79f8328
keiwebcreate <keiwebcreate20240704@gmail.com>
2026-06-02 14:09:53 +0900

    1対多のリレーションを設定しました。

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\Admin\StoreBlogRequest;
 use App\Http\Requests\Admin\UpdateBlogRequest;
 use App\Models\Blog;
+use App\Models\Category;
 use Illuminate\Support\Facades\Storage;
 
 class AdminBlogController extends Controller
@@ -55,7 +56,8 @@ class AdminBlogController extends Controller
      */
     public function edit(Blog $blog)
     {
-        return view('admin.blogs.edit', ['blog' => $blog]);
+        $categories = Category::all();
+        return view('admin.blogs.edit', ['blog' => $blog, 'categories' => $categories]);
     }
 
     /**
@@ -73,7 +75,7 @@ class AdminBlogController extends Controller
             // 変更後の画像をアップロード、保存パスを更新対象データにセット
             $updateData['image'] = $request->file('image')->store('blogs'. 'public');
         }
-
+        $blog->category()->associate($updateData['category_id']);
         $blog->update($updateData);
 
         return to_route('admin.blogs.index')->with('success', 'ブログを更新しました');
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Blog extends Model
 {
-    protected $fillable = ['title', 'body'];
+    protected $fillable = ['title', 'body', 'category_id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
