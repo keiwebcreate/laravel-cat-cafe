ad3b9305dcc38977c26a4b5f7a3ed570f725fa1c
keiwebcreate <keiwebcreate20240704@gmail.com>
2026-06-03 18:33:07 +0900

    ログイン時にログイン画面が表示されないようにしました。

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/contact/complete', [ContactController::class, 'complete'])->name('c
 Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
 Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
 Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
+Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit')->middleware('auth');
 Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
 Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
 
@@ -30,6 +30,6 @@ Route::get('/admin/users/create', [UserController::class, 'create'])->name('admi
 Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
 
 // ユーザー認証
-Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login')->middleware('guest');
 Route::post('/admin/login', [AuthController::class, 'login']);
 Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
